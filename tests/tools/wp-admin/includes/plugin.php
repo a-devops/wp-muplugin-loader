286274b1501c8751876b187e5f98229d5e672be5
Boris Gorbylev <ekho@ekho.name>
2022-03-10 20:58:35 +0300

    Fix getMuPath

--- tests/tools/wp-admin/includes/plugin.php
+++ tests/tools/wp-admin/includes/plugin.php
@@ -2,11 +2,13 @@
 /**
  * A helper for testing to make sure this file is includable.
  */
+
 namespace LkWdWrd\Test\WP_Admin;
+
 use WP_Mock;
 
 // Set the included flag.
 const included = true;
 
 // Mock the get_plugins function to return an empty array.
-WP_Mock::userFunction( 'get_plugins', [ 'return' => [] ] );
+WP_Mock::userFunction('get_plugins', [ 'return' => [] ]);
